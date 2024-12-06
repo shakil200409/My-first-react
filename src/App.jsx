226aa84f81e56cc6aaccb8dafd8ea4ed7230646d
Shakil Ahmed <shakil200409@gmail.com>
2024-12-06 22:52:55 +0600

    Learned to declare a component dinamically in jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,25 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
-      
+      <Person></Person>
+      <Student></Student>
     </>
   )
 }
 
+function Person(){
+  const person = {name: 'Shakil Ahmed', age: 20};
+  const money = 100;
+  return <h3>I am {person.name}, my age is {person.age}. Total amount of balance on my pocket is {money}. </h3>
+}
+
+const Student = () =>{
+  const student = {id: '01', name: 'Shakil Ahmed', subject: 'CSE'}
+  return <div>
+    <h2>This is a student</h2>
+    <p>Name: {student.name} </p>
+    <p>Subject: {student.subject} </p>
+    <p>Id: {student.id} </p>
+  </div>
+}
 export default App
