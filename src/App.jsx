0c8ce49f4a6c3bc63299c983db28a8e01441c1e7
Shakil Ahmed <shakil200409@gmail.com>
2024-12-07 01:05:08 +0600

    Learned to create component and adding data dinamically to there

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,44 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <Developer></Developer>
+      <Device name="laptop" price="10000"></Device>
+      <Device name="Mobile" price="5000"></Device>
+      <Device name="Monitor" price="20000"></Device>
       <Person></Person>
       <Student></Student>
     </>
   )
 }
 
+function Device(props){
+  console.log(props)
+  return <h3>Device: {props.name}, Price: {props.price} </h3>
+}
+
+function Developer(){
+const devStyle = {
+  border: '2px solid tomato',
+  borderRadius: '10px',
+  backgroundColor: 'black',
+  color: 'green'
+}
+
+  return <div style={devStyle}>
+    <h2>Developer</h2>
+    <p>Dev Dev</p>
+  </div>
+}
+
 function Person(){
   const person = {name: 'Shakil Ahmed', age: 20};
   const money = 100;
-  return <h3>I am {person.name}, my age is {person.age}. Total amount of balance on my pocket is {money}. </h3>
+  return <h3 style={{backgroundColor: 'white', color: 'black', padding: '15px', margin: '20px', fontSize: '20px', borderRadius: '10px'}}>I am {person.name}, my age is {person.age}. Total amount of balance on my pocket is {money}. </h3>
 }
 
 const Student = () =>{
   const student = {id: '01', name: 'Shakil Ahmed', subject: 'CSE'}
-  return <div>
+  return <div className='student'>
     <h2>This is a student</h2>
     <p>Name: {student.name} </p>
     <p>Subject: {student.subject} </p>
