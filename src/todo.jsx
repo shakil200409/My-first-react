6393c16e5f0d05c2b4f7c45bd4173d2a42a32f1c
Shakil Ahmed <shakil200409@gmail.com>
2024-12-08 01:18:03 +0600

    Learned conditional rendaqring with ternary operator, and operator, or operator

--- src/todo.jsx
+++ src/todo.jsx
@@ -20,9 +20,30 @@
 // }
 
 // Conditional Rendaring: Option-2
+// export default function Todo({task, isDone}){
+//     if(isDone){
+//         return <li>Finished: {task}</li>
+//     }
+//     return <li>Work on: {task}</li>
+// }
+
+// // Conditional Rendaring: Option-3 Tarnery Operator
+// export default function Todo({task, isDone}){
+//     return(
+//         <li> {isDone ? 'Finished' : 'Work on'} : {task} </li>
+//     )
+// }
+
+// // Conditional Rendaring: Option-4 With and operator- &&
+// export default function Todo({task, isDone}){
+//     return(
+//         <li> {task}: {isDone && ' Done'} </li>
+//     )
+// }
+
+// Conditional Rendaring: Option-4 With or operator- ||
 export default function Todo({task, isDone}){
-    if(isDone){
-        return <li>Finished: {task}</li>
-    }
-    return <li>Work on: {task}</li>
+    return(
+        <li> {task}: {isDone || ' Do it'} </li>
+    )
 }
